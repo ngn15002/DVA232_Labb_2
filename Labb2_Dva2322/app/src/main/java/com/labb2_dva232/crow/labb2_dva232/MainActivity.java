07b5a401fdbe11e3ed7640cf6c5789f57aecf782
Crowfather <Triggertown@hotmail.com>
2016-12-13 16:46:27 +0100

    tt

--- Labb2_Dva2322/app/src/main/java/com/labb2_dva232/crow/labb2_dva232/MainActivity.java
+++ Labb2_Dva2322/app/src/main/java/com/labb2_dva232/crow/labb2_dva232/MainActivity.java
@@ -1,13 +1,31 @@
 package com.labb2_dva232.crow.labb2_dva232;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    private void dispatchTakePictureIntent() {
+
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+
+    }
+
 }
